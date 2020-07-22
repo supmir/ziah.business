5b3631233824897810e41419ab92c1d2451f9a5c
supmir <iskandar.amir@gmail.com>
2020-07-22 20:19:23 +0800

    Fixed wgi.py

--- ziahbusiness/wsgi.py
+++ ziahbusiness/wsgi.py
@@ -7,10 +7,21 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
+# =====================
+# wsgi.py file begin 
+
+import os, sys
+# add the hellodjango project path into the sys.path
+sys.path.append('/home/iskandar_amir/ziah.business/ziahbusiness')
+
+# add the virtualenv site-packages path to the sys.path
+sys.path.append('/home/iskandar_amir/ziah.business/ziahbusinessenv/Lib/site-packages')
+
+# poiting to the project settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ziahbusiness.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ziahbusiness.settings')
-
 application = get_wsgi_application()
+
+# wsgi.py file end
+# ===================
