e53d439167937a571ce2aab6985387ad5f6bab7e
supmir <iskandar.amir@gmail.com>
2020-07-22 23:05:15 +0800

    fixed installedapps

--- ziahbusiness/settings.py
+++ ziahbusiness/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ["34.125.195.39","ziah.business"]
 # Application definition
 
 INSTALLED_APPS = [
-    'v1.apps.V1',
+    'v1.apps.V1Config',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
