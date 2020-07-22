ecd9bb23d7b5eb85d85729573011d3b8f672c55a
supmir <iskandar.amir@gmail.com>
2020-07-22 23:05:54 +0800

    removed unavailable urls

--- v1/urls.py
+++ v1/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     
     path('', views.products, name='products'),
     # path('DearCovid', RedirectView.as_view(url = "http://19-ncov.cf/DearCovid/1"), name='messages'),
-    path('Maxxima/<str:productname>', views.maxxima, name='maximma'),
-    path('Garden/<str:productname>', views.garden, name='garden'),
+    # path('Maxxima/<str:productname>', views.maxxima, name='maximma'),
+    # path('Garden/<str:productname>', views.garden, name='garden'),
     # path('<str:pagename>', views.multiview),
 ]
