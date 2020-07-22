d9b9f3e81835a81ee1429e890c7642f60e3f948c
supmir <iskandar.amir@gmail.com>
2020-07-22 23:02:21 +0800

    added views.py in main

---
+++ ziahbusiness/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+# class IndexView(generic.ListView):
+#     template_name = 'polls/index.html'
+
+#     def get_queryset(self):
+#         """Return the last five published questions."""
+#         return Question.objects.order_by('-pub_date')[:5]
