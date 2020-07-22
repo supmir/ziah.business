978936214bcbb93a0e0bd131eda6b64120157dad
supmir <iskandar.amir@gmail.com>
2020-07-22 20:31:34 +0800

    wsgi unfixed(didnt need fixing)

--- ziahbusiness/wsgi.py
+++ ziahbusiness/wsgi.py
@@ -7,21 +7,28 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-# =====================
-# wsgi.py file begin 
-
-import os, sys
-# add the hellodjango project path into the sys.path
-sys.path.append('/home/iskandar_amir/ziah.business/ziahbusiness')
-
-# add the virtualenv site-packages path to the sys.path
-sys.path.append('/home/iskandar_amir/ziah.business/ziahbusinessenv/Lib/site-packages')
-
-# poiting to the project settings
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ziahbusiness.settings")
+import os
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
 
-# wsgi.py file end
-# ===================
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ziahbusiness.settings')
+
+application = get_wsgi_application()
+# # =====================
+# # wsgi.py file begin 
+
+# import os, sys
+# # add the hellodjango project path into the sys.path
+# sys.path.append('/home/iskandar_amir/ziah.business/ziahbusiness')
+
+# # add the virtualenv site-packages path to the sys.path
+# sys.path.append('/home/iskandar_amir/ziah.business/ziahbusinessenv/Lib/site-packages')
+
+# # poiting to the project settings
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ziahbusiness.settings")
+
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
+
+# # wsgi.py file end
+# # ===================
