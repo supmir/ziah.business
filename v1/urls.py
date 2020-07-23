256f37923eb96715383c33023ee4238296ec7983
supmir <iskandar.amir@gmail.com>
2020-07-23 11:16:40 +0800

    Added category button

--- v1/urls.py
+++ v1/urls.py
@@ -6,6 +6,7 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
     
     path('', views.products, name='products'),
+    path('<str:category>', views.products, name='products'),
     # path('DearCovid', RedirectView.as_view(url = "http://19-ncov.cf/DearCovid/1"), name='messages'),
     path('<str:category>/<str:productname>', views.product, name='product'),
     # path('<str:pagename>', views.multiview),
