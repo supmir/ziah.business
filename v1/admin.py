55a42938599ee2ea5bb517499c4c521aa16475f8
supmir <iskandar.amir@gmail.com>
2020-07-23 10:10:18 +0800

    Register Category

--- v1/admin.py
+++ v1/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Product,Content
+from .models import Product,Content,Category
 # Register your models here.
 
 admin.site.register(Product)
 admin.site.register(Content)
+admin.site.register(Category)
